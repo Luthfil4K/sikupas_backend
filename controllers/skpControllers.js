0e108fed6f7f72f343362e69e8adc4d5ad0f5c24
Luthfil4K <akaishuichi141516@gmail.com>
2025-04-13 22:48:24 +0800

    dashboard card

---
+++ controllers/skpControllers.js
@@ -0,0 +1,19 @@
+import prisma from "../prisma/client";
+
+const getAllSKP = async (req, res) => {
+    console.log('Masuk ke getAllSKP controller');
+    console.log('Masuk ke getAllSKP controller');
+  try {
+    const dataSKP = await prisma.skp_skp.findMany({
+      include: {
+        pegawai: true,
+      },
+    });
+    console.log()
+    res.status(200).json(dataSKP);
+  } catch (err) {
+    res.status(500).json({ error: "Gagal mengambil data pegawai" });
+  }
+};
+
+export default getAllSKP;
---
+++ pages/api/skp/index.js
@@ -0,0 +1,22 @@
+import NextCors from 'nextjs-cors';
+import getAllSKP from '../../../controllers/skpControllers';
+
+export default async function handler(req, res) {
+  await NextCors(req, res, {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // ✅ Tambahkan ini
+  });
+
+  if (req.method === 'GET') {
+    try {
+      const data = await getAllSKP(req, res);
+      console.log(data)
+      res.status(200).json(data);
+    } catch (err) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
