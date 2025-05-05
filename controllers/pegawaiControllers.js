aa67024cc44fb7176c02d563fe66bd80784c31de
Luthfil4K <akaishuichi141516@gmail.com>
2025-05-05 11:48:29 +0800

    perbaikan tampilan

--- controllers/pegawaiControllers.js
+++ controllers/pegawaiControllers.js
@@ -1,16 +1,34 @@
 import prisma from "../prisma/client";
 
 const getAllPegawai = async (req, res) => {
+
+
+  const bulanIni = new Date();
+const awalBulan = new Date(bulanIni.getFullYear(), bulanIni.getMonth(), 1);
+const akhirBulan = new Date(bulanIni.getFullYear(), bulanIni.getMonth() + 1, 0);
+
   try {
     const dataPegawai = await prisma.pegawai.findMany({
       include: {
         timkerja: true,
+        ckp: {
+          where: {
+            tglMulai: {
+              gte: awalBulan,
+            },
+          },
+          select: {
+            progress: true, // hanya ambil kolom progress
+          },
+        },
       },
     });
+    
     console.log()
     res.status(200).json(dataPegawai);
   } catch (err) {
-    res.status(500).json({ error: "Gagal mengambil data pegawai" });
+    console.log(err)
+    res.status(500).json({ error: "Gagal mengambil data pegawai"+err });
   }
 };
 
